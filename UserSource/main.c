8a4b3cb9d7b5ca995711fb1ea4ce0587aea73269
T-1000\brainiac <igrafskiy@gmail.com>
2016-08-06 18:48:47 +0300

    PWM1 Output compare port B6 servo

--- UserSource/main.c
+++ UserSource/main.c
@@ -7,6 +7,7 @@ int dir_flag = 1;
 int main(void)
 {
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; //gpiod enable
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOBEN; // gpiob enable;
 	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN;//tim4 enable
 	GPIOD->MODER |= GPIO_MODER_MODER14_0;// | GPIO_MODER_MODER15_0; // as output
 	GPIOD->OSPEEDR = GPIO_OSPEEDER_OSPEEDR14_0;// very high speed
@@ -15,6 +16,11 @@ int main(void)
 	
 	GPIOD->ODR = GPIO_ODR_ODR_14;// led on
 	
+	GPIOB->MODER |= GPIO_MODER_MODER6_1; // PB6 AF
+	GPIOB->OSPEEDR |= GPIO_OSPEEDER_OSPEEDR6_0; // very high speed
+	GPIOB->OTYPER &= ~GPIO_OTYPER_OT_6;//push pull
+	GPIOB->PUPDR &= ~GPIO_PUPDR_PUPDR6; // no pull
+	GPIOB->AFR[0] |= (2 << 24);
 	
 	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOHEN;
 	GPIOH->MODER |= GPIO_MODER_MODER0_0; // H0 as out
@@ -27,49 +33,14 @@ int main(void)
 	TIM4->PSC = 10-1; //prescaler
 	TIM4->CR1 &= ~TIM_CR1_DIR; // direction up
 	TIM4->CR1 |= TIM_CR1_CEN;
+	
+	TIM4->CCMR1 |= TIM_CCMR1_OC1M_1 | TIM_CCMR1_OC1M_2 | TIM_CCMR1_OC1PE | TIM_CCMR1_OC1FE; //pwm mode 1
+	TIM4->CCR1 = 4028;
+	TIM4->CCER |= TIM_CCER_CC1E;
+	TIM4->EGR |= TIM_EGR_UG;
 		while(1)
 		{
-
 			
-			if(TIM4->CNT > servo_value)
-			{
-				i++;
-				GPIOD->ODR &= ~GPIO_ODR_ODR_14;
-				GPIOH->ODR &= ~GPIO_ODR_ODR_0;
-			}
-				else
-			{
-				GPIOH->ODR |= GPIO_ODR_ODR_0;
-				GPIOD->ODR |= GPIO_ODR_ODR_14;
-			} 
-			if(dir_flag)
-			{
-				if(servo_value > 5318)
-				{
-					dir_flag = 0;
-				}
-				else
-				{
-					servo_value += 3;
-				}
-			}
-			else
-			{
-					if(servo_value < 1611 )
-				{
-					dir_flag = 1;
-				}
-				else
-				{
-					servo_value -= 3;
-				}
-			}
-					//for(i = 0; i < 1000000; i++);
-			//GPIOD->ODR = GPIO_ODR_ODR_14;
-			//GPIO_SetBits(GPIOD, GPIO_Pin_14);
-			//for(i = 0; i < 1000000; i++);
-			//GPIOD->ODR &= ~GPIO_ODR_ODR_14;
-			//GPIO_ResetBits(GPIOD, GPIO_Pin_14);
 		}
 	
 }
